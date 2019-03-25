90c5456774b71162aabb96188890c093fbef0b17
Ititi Obidah <iobidah9@gmail.com>
2019-03-25 15:54:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,7 @@ def fizzbuzz(num)
    return "Fizz"
    elsif num % 5 == 0 
     return "Buzz"
-    elsif num % 15 == 0 
+    else num % 15 == 0 
     return "FizzBuzz"
-  else num % 4 == 0 
-    return nil
 end
 end
