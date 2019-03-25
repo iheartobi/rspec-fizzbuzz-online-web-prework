a41ddc8cc0636c48e3776a4f4cf2524f0467bcbe
Ititi Obidah <iobidah9@gmail.com>
2019-03-25 16:00:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,7 @@
+require 'pry'
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'pry'
+
 
 def fizzbuzz(num)
   if num % 3 == 0 
